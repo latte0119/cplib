5625dc1b8d712ade2741af34fdcbcaab8f1bc4c9
latte0119 <latte.malta@gmail.com>
2020-09-06 17:05:42 +0900

    zalgo

---
+++ zalgorithm.test.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#include "string/zalgorithm.hpp"
+
+using namespace std;
+
+int main() {
+    string S;
+    cin >> S;
+    auto a = zalgorithm(S);
+    for (int i = 0; i < S.size(); i++) {
+        if (i) cout << " ";
+        cout << a[i];
+    }
+    cout << endl;
+}
